0b68871d1d9b495ce28085dea1a3722f4038f0eb
NicholasCarlon <nec56@nau.edu>
2023-07-26 19:29:26 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 # These plots include checkboxes to eliminate or show outliers in each visual
 #SHOWING THAT CHANGING THIS PYTHON SCRIPT WORKS FOR GITHUB
 # import necessary packages
-
+#SECOND UPDATE OF MY CODE FOR MY WEBSITE CHECKING ON GITHUB
 import streamlit as st
 import pandas as pd
 import plotly_express as px
